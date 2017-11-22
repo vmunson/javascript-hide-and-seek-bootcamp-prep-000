18f3f6fed4974ed1624b5961660589d2ceb90949
Ginny Munson <virginia.munson@gmail.com>
2017-11-22 19:32:34 +0000

    Done.

--- index.js
+++ index.js
@@ -9,9 +9,21 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  var list = document.querySelectorAll(`.ranked-list`)
+  var list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   for(let i = 0; i < list.length; i++){
-    var element = list[i].innerHTML + n
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n
   }
-  return element
+  return list
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
 }
